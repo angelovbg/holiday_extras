2b32db73ffe111cd1c0c79404efe886bf6c1f345
Angel Angelov <aangelov@atomsolutions.net>
2018-12-23 15:50:32 +0200

    environment added

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Holiday Extras
 CRUD user API
+
+Install --global typescript
+Add to environment path if needed
+
+generate .env file in project folder with content form .env.dist
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,32 @@
 import * as express from 'express';
+import * as dotenv from 'dotenv';
+import { Request, Router } from 'express';
 
 class Server {
     public app: express.Application;
 
     public constructor() {
+        dotenv.config({path: '.env'});
+
         this.app = express();
 
         this.config();
+        this.routes();
     }
 
     public config(): void {
-        this.app.set('port', 3000);
+        this.app.set('port', process.env.APP_PORT || 3000);
 
         this.app.listen(this.app.get('port'), () => {
             console.log(('App is running at http://localhost:%d'), this.app.get('port'));
             console.log('Press CTRL-C to stop\n');
         });
+    }
+
+    public routes(): void {
+        const router = express.Router();
+
+        this.app.use('/', router);
     }
 }
 
